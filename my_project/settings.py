0a77abe624f48b13d26cf3f06a9f9e6eecf4703d
Matheus Vanzan <vanzan2015@gmail.com>
2016-05-20 17:47:41 -0400

    Fix STATIC and MEDIA variables

--- my_project/settings.py
+++ my_project/settings.py
@@ -85,9 +85,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
 STATIC_URL = '/static/'
-
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+STATIC_ROOT = os.path.join(BASE_DIR, '../static/' + PROJECT_NAME + '/static')
 
 MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, '../media/' + PROJECT_NAME + '/static')
 
 LOGIN_URL = '/login/'
